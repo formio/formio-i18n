3c6f76e200e000d4c0f7faed6f3b179155171d0d
RAVI KACHHAWAHA <ravi.kachhawaha@qmcpl.com>
2016-04-07 18:56:13 +0530

    manage code spacing

--- src/formio-translate.js
+++ src/formio-translate.js
@@ -18,13 +18,13 @@ angular.module('ngformioTranslate', ['pascalprecht.translate','tmh.dynamicLocale
     replace: true,
     template: ''+
     '<div class="language-select" ng-if="visible">'+
-        '<label>'+
-            '{{"directives.language-select.Language" | translate}}:'+
-            '<select ng-model="currentLocaleDisplayName"'+
-                'ng-options="localesDisplayName for localesDisplayName in localesDisplayNames"'+
-                'ng-change="changeLanguage(currentLocaleDisplayName)">'+
-            '</select>'+
-        '</label>'+
+      '<label>'+
+        '{{"directives.language-select.Language" | translate}}:'+
+        '<select ng-model="currentLocaleDisplayName"'+
+          'ng-options="localesDisplayName for localesDisplayName in localesDisplayNames"'+
+          'ng-change="changeLanguage(currentLocaleDisplayName)">'+
+        '</select>'+
+      '</label>'+
     '</div>'+
     '',
     controller: function ($scope) {
@@ -37,64 +37,63 @@ angular.module('ngformioTranslate', ['pascalprecht.translate','tmh.dynamicLocale
     }
   };
 })
-  .service('LocaleService', function ($translate, LOCALES, $rootScope, tmhDynamicLocale, $state) {
-    'use strict';
-    // PREPARING LOCALES INFO
-    var localesObj = LOCALES.locales;
+.service('LocaleService', function ($translate, LOCALES, $rootScope, tmhDynamicLocale, $state) {
+  'use strict';
+  // PREPARING LOCALES INFO
+  var localesObj = LOCALES.locales;
 
-    // locales and locales display names
-    var _LOCALES = Object.keys(localesObj);
-    if (!_LOCALES || _LOCALES.length === 0) {
-      console.error('There are no _LOCALES provided');
-    }
-    var _LOCALES_DISPLAY_NAMES = [];
-    _LOCALES.forEach(function (locale) {
-      _LOCALES_DISPLAY_NAMES.push(localesObj[locale]);
-    });
-    
-    // STORING CURRENT LOCALE
-    var currentLocale = $translate.proposedLanguage();// because of async loading
-    
-    // METHODS
-    var checkLocaleIsValid = function (locale) {
-      return _LOCALES.indexOf(locale) !== -1;
-    };
-    
-    var setLocale = function (locale) {
-      if (!checkLocaleIsValid(locale)) {
-        console.error('Locale name "' + locale + '" is invalid');
-        return;
-      }
-      currentLocale = locale;// updating current locale
-     
-      // asking angular-translate to load and apply proper translations
-     
-      $translate.use(locale);
-      $state.reload();
-    };
-    
-    // EVENTS
-    // on successful applying translations by angular-translate
-    $rootScope.$on('$translateChangeSuccess', function (event, data) {
-      document.documentElement.setAttribute('lang', data.language);// sets "lang" attribute to html
-    
-       // asking angular-dynamic-locale to load and apply proper AngularJS $locale setting
-      tmhDynamicLocale.set(data.language.toLowerCase().replace(/_/g, '-'));
-    });
-    
-    return {
-      getLocaleDisplayName: function () {
-        return localesObj[currentLocale];
-      },
-      setLocaleByDisplayName: function (localeDisplayName) {
-        return setLocale(
-          _LOCALES[
-            _LOCALES_DISPLAY_NAMES.indexOf(localeDisplayName)// get locale index
-            ]
-        );
-      },
-      getLocalesDisplayNames: function () {
-        return _LOCALES_DISPLAY_NAMES;
-      }
-    };
+  // locales and locales display names
+  var _LOCALES = Object.keys(localesObj);
+  if (!_LOCALES || _LOCALES.length === 0) {
+    console.error('There are no _LOCALES provided');
+  }
+  var _LOCALES_DISPLAY_NAMES = [];
+  _LOCALES.forEach(function (locale) {
+    _LOCALES_DISPLAY_NAMES.push(localesObj[locale]);
   });
+  
+  // STORING CURRENT LOCALE
+  var currentLocale = $translate.proposedLanguage();// because of async loading
+  
+  // METHODS
+  var checkLocaleIsValid = function (locale) {
+    return _LOCALES.indexOf(locale) !== -1;
+  };
+  
+  var setLocale = function (locale) {
+    if (!checkLocaleIsValid(locale)) {
+      console.error('Locale name "' + locale + '" is invalid');
+      return;
+    }
+    currentLocale = locale;// updating current locale
+   
+    // asking angular-translate to load and apply proper translations
+    $translate.use(locale);
+    $state.reload();
+  };
+  
+  // EVENTS
+  // on successful applying translations by angular-translate
+  $rootScope.$on('$translateChangeSuccess', function (event, data) {
+    document.documentElement.setAttribute('lang', data.language);// sets "lang" attribute to html
+  
+     // asking angular-dynamic-locale to load and apply proper AngularJS $locale setting
+    tmhDynamicLocale.set(data.language.toLowerCase().replace(/_/g, '-'));
+  });
+  
+  return {
+    getLocaleDisplayName: function () {
+      return localesObj[currentLocale];
+    },
+    setLocaleByDisplayName: function (localeDisplayName) {
+      return setLocale(
+        _LOCALES[
+          _LOCALES_DISPLAY_NAMES.indexOf(localeDisplayName)// get locale index
+          ]
+      );
+    },
+    getLocalesDisplayNames: function () {
+      return _LOCALES_DISPLAY_NAMES;
+    }
+  };
+});
